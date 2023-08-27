5a3e3410ffca1ca8fb37f35368d82f51a892b4f8
dev <dev@TeamCity>
2023-08-27 23:49:14 +0000

    TeamCity change in 'Root Teamcity Pipeline / projectexp / backend' project: parameters of 'publish' build configuration were updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -127,7 +127,7 @@ object ProtomathTeamcityPipeline_Projectexp_Backend_Publish : BuildType({
 
     params {
         text("CURRENT_TAG", "", display = ParameterDisplay.PROMPT, allowEmpty = true)
-        param("SKIP_PUBLISH", "FALSE")
+        param("SKIP_PUBLISH", "false")
     }
 
     vcs {
