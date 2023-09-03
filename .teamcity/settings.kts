95f2992e4943f65cf0c910d4f05548d0577ec261
dev <dev@TeamCity>
2023-09-03 17:38:35 +0000

    TeamCity change in 'Root Teamcity Pipeline / projectexp / backend' project: runners of 'build' build configuration were updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -154,8 +154,9 @@ object ProtomathTeamcityPipeline_Projectexp_Backend_Build : BuildType({
                 npm install
                 npm install @semantic-release/git @semantic-release/changelog -D
                 
-                echo HELP
-                GH_TOKEN=%env.GITHUB_TOKEN% npx semantic-release --no-ci
+                echo HELP 
+                echo ${'$'}{%env.GITHUB_TOKEN%}
+                GH_TOKEN=${'$'}{%env.GITHUB_TOKEN%} npx semantic-release --no-ci
             """.trimIndent()
         }
     }
