5d90708120084ff0a04bbba046356ec3547aad32
dev <dev@TeamCity>
2023-09-03 15:39:37 +0000

    TeamCity change in 'Root Teamcity Pipeline / projectexp / backend' project: runners of 'build' build configuration were updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -144,7 +144,7 @@ object ProtomathTeamcityPipeline_Projectexp_Backend_Build : BuildType({
             name = "build"
             shellScript = """
                 npm install
-                npx semantic-release --no-ci --repository-url=https://github.com/ProtoMath2021/project-expert-backend.git
+                GH_TOKEN=DslContext.getParameter(GH_TOKEN, 1) npx semantic-release --no-ci --repository-url=https://github.com/ProtoMath2021/project-expert-backend.git
             """.trimIndent()
         }
     }
