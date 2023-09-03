4d6f2d21402ae781ee3103b48709d765980d487e
dev <dev@TeamCity>
2023-09-03 19:48:41 +0000

    TeamCity change in 'Root Teamcity Pipeline / projectexp / backend' project: runners of 'build' build configuration were updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -151,7 +151,7 @@ object ProtomathTeamcityPipeline_Projectexp_Backend_Build : BuildType({
                 npm install @semantic-release/git @semantic-release/changelog -D
                 npm update semantic-release @semantic-release/* --save-dev
                 git config --global --add safe.directory "${'$'}(pwd)"
-                npm install java
+                npm install gradle --save-dev
                 
                 echo HELP 
                 echo %build.number%
