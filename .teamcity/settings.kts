7d56f5134a4f5f74a0498e5b5b3c563af67a10b1
dev <dev@TeamCity>
2023-09-11 18:33:00 +0000

    TeamCity change in 'Root Teamcity Pipeline / projectexp / backend' project: runners of 'build' build configuration were updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -189,7 +189,7 @@ object ProtomathTeamcityPipeline_Projectexp_Backend_Build : BuildType({
         script {
             scriptContent = """
                 echo "`ls -la ./out/`"
-                mv ./out/*.jar ./out/amogus-%env.CURRENT_TAG_EXPERT%
+                mv ./out/*.jar ./out/amogus-%env.CURRENT_TAG_EXPERT%.jar
                 
                 find ./out -name '*.jar' -type f -delete
                 echo "`ls -la ./out/`"
