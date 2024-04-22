42bb0dd7adb3eef6271ef2a1488118fc35cd6438
Murat Slavich <slavichmo@gmail.com>
2024-04-22 19:56:50 +0300

    delete

--- .teamcity/ProjectExpert/Project.kt
+++
@@ -1,9 +0,0 @@
-package ProjectExpert
-
-import jetbrains.buildServer.configs.kotlin.*
-import jetbrains.buildServer.configs.kotlin.Project
-
-object Project : Project({
-    id("ProjectExpert")
-    name = "ProjectExpert"
-})
--- .teamcity/ProtonMath_Backend/Project.kt
+++
@@ -1,9 +0,0 @@
-package ProtonMath_Backend
-
-import jetbrains.buildServer.configs.kotlin.*
-import jetbrains.buildServer.configs.kotlin.Project
-
-object Project : Project({
-    id("ProtonMath_Backend")
-    name = "Backend"
-})
