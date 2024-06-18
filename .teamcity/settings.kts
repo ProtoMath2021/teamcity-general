9ae0f48d5394744f4e15ff1db3b0add0e34b5040
dev <dev@TeamCity>
2024-06-18 15:48:03 +0000

    TeamCity change in 'ProtonMath / Backend' project: runners of 'build' build configuration were updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -123,7 +123,7 @@ object ProtonMath_Backend_Build : BuildType({
                 
                 echo "`ls -la`"
             """.trimIndent()
-            dockerImage = "node:21.7.3-slim"
+            dockerImage = "node"
             dockerPull = true
         }
         script {
