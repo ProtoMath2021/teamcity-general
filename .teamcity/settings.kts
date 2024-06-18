dc789446b3f6c2f24d35ec90a832f2cc43ff2bbc
dev <dev@TeamCity>
2024-06-18 15:54:00 +0000

    TeamCity change in 'ProtonMath / Backend' project: runners of 'build' build configuration were updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -107,6 +107,7 @@ object ProtonMath_Backend_Build : BuildType({
                 
                 echo HELP1
                 
+                git config --global --add safe.directory "${'$'}{'${'$'}'}(pwd)"
                 echo HELP 
                 echo %build.number%
                 echo %env.GH_TOKEN%
