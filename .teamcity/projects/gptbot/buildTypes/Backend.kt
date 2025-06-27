7e40c1fe5d5a646bccd929cc2c21389359a092f0
Murat Slavich <slavichmo@gmail.com>
2025-06-27 12:38:51 +0300

    Refactor Docker support feature configuration in Backend build type

--- .teamcity/projects/gptbot/buildTypes/Backend.kt
+++ .teamcity/projects/gptbot/buildTypes/Backend.kt
@@ -45,11 +45,11 @@ object Backend : BuildType({
     }
 
     features {
-        dockerSupport {
-            cleanupPushedImages = true
-            loginToRegistry = on {
-                dockerRegistryId = "PROJECT_EXT_3"
-            }
+        feature {
+            type = "DockerSupport"
+            param("cleanupPushedImages", "true")
+            param("loginToRegistry", "true")
+            param("dockerRegistryId", "PROJECT_EXT_3")
         }
     }
 
