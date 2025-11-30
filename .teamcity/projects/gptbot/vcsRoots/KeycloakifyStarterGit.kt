6ad21316a92bfe2375d9785c1b4ab9d56b88e955
Murat Slavich <slavichmo@gmail.com>
2025-12-01 00:29:14 +0300

    Fix KeycloakifyStarter VCS root to use GitHub App token auth

--- .teamcity/projects/gptbot/vcsRoots/KeycloakifyStarterGit.kt
+++ .teamcity/projects/gptbot/vcsRoots/KeycloakifyStarterGit.kt
@@ -4,12 +4,12 @@ import jetbrains.buildServer.configs.kotlin.vcs.GitVcsRoot
 
 object KeycloakifyStarterGit : GitVcsRoot({
     id("Gptbot_KeycloakifyStarterGit")
-    name = "git@github.com:dev4team-ai/keycloakify-starter.git"
-    url = "git@github.com:dev4team-ai/keycloakify-starter.git"
+    name = "https://github.com/dev4team-ai/keycloakify-starter#refs/heads/master"
+    url = "https://github.com/dev4team-ai/keycloakify-starter"
     branch = "refs/heads/master"
-    branchSpec = "*"
-    authMethod = uploadedKey {
-        userName = "git"
-        uploadedKey = "gpt-agent-ui"
+    branchSpec = "refs/heads/*"
+    authMethod = token {
+        userName = "oauth2"
+        tokenId = "tc_token_id:CID_5d39b539391852e509211819e6a59e55:-1:5a458da9-ef72-4eba-bc5f-44a60d22969b"
     }
 })
